bd1cdf8f390888645133508dee98e87bc154db4c
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-06-07 21:15:36 +0530

    user model, validating user input

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-
-router.get('/', (req,res) => res.send('User route'));
+router.post('/',[
+    check('name', 'Name is required')
+    .not()
+    .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+    .isLength({ min: 6 })
+], 
+    (req,res) => {
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    // data sent to this route gets printed in the console
+    console.log(req.body)
+    res.send('User route');
+});
 
 module.exports = router;
