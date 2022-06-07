14ef20097f481ef82b388c53e28b14afc68d77e9
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-06-07 17:08:29 +0530

    routes

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const app = express();
 
 // Connect database
 connectDB();
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth')) 
+app.use('/api/profile', require('./routes/api/profile')) 
 
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => res.send('API Running'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
