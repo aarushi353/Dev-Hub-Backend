e0d66d9f792be3041a4be47f4491a67f4b379ae2
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-06-08 17:50:01 +0530

    listing all profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -89,4 +89,15 @@ router.post('/', [ auth , [
   
 })
 
+router.get('/', async (req, res) => {
+   try {
+       const profiles = await Profile.find().populate('user',['name','avatar']);
+       res.json(profiles);
+   } catch (error) {
+       console.error(error.message);
+       res.status(500).send('Server Error');
+   }
+})
+
+
 module.exports = router;
