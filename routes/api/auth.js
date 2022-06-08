e87a9fc8469ae1e0a13dab12d900191591899b51
Aarushi <72608824+aarushi353@users.noreply.github.com>
2022-06-08 16:13:38 +0530

    profile model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 router.get('/', auth, async (req,res) => {
     try{
+        // we don't want the password so removed it
         const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     } catch(err){
@@ -12,5 +17,53 @@ router.get('/', auth, async (req,res) => {
       res.status(500).send('Server Error');
     }
 });
+
+
+router.post('/',[
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+    .exists()
+], 
+    async (req,res) => {
+    // checking if the user exists
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    
+    try{
+    let user = await User.findOne({ email });
+
+    if(!user){
+       return res.status(400).json({ errors: [{ msg: 'Invalid credentials'}]});
+    }
+     
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({errors: [{ msg: 'Invalid Credentials'}]});
+    }
+
+    const payload = {
+        user: {
+            id: user.id,
+
+        }
+    }
+
+    jwt.sign(payload, config.get('jwtSecret'),
+    {
+        expiresIn: 360000
+    },(err, token) => {
+        if(err) throw err;
+        res.json({ token });
+    });
+} catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,68 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const mongoose = require('mongoose');
+const { check, validationResult } = require('express-validator');
 
+router.get('/me', auth, async (req,res) => {
+   try{
+    const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+  
+      if(!profile){
+          return res.status(400).json({ msg: 'There is no profile for this user'});
+      }
+      res.json(profile);
+    } catch (err){
+       console.error(err.message);
+       res.status(500).send('Server Error');
+   }
+});
 
-router.get('/', (req,res) => res.send('Profile route'));
+router.post('/', [ auth , [
+    check('status', 'Status id required').not().isEmpty(),
+    check('skills','Skills id required').not().isEmpty()
+]], async (req, res) => {
+    const errors  = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+     const {
+        company,
+        website,
+        location,
+        bio,
+        status,
+        githubusername,
+        skills,
+        youtube,
+        twitter,
+        instagram,
+        linkedin,
+        facebook,
+      } = req.body;
+
+      // Build profile object
+      const profileFields = {};
+      profileFields.user = req.user.id;
+      if(company) profileFields.company = company;
+      if(website) profileFields.website = website;
+      if(location) profileFields.location = location;
+      if(bio) profileFields.bio = bio;
+      if(status) profileFields.status = status;
+      if(githubusername) profileFields.githubusername = githubusername;
+      if(skills){
+          profileFields.skills = skills.split(',').map(skill => skill.trim());
+      }
+
+      console.log(profileFields.skills);
+
+      res.send('Hello');
+
+  
+  
+})
 
 module.exports = router;
